e60e90f7dd37d4fc097e28897c52203bc800c59b
LeonelNL <leonelnieto305@gmail.com>
2023-04-12 10:40:34 -0600

    Adding definitions

--- src/definitions.py
+++ src/definitions.py
@@ -1,5 +1,7 @@
-def DATA_TO_WRITE(Data):
-    Erd_CurrentSystemState = Data[0]
+def ERDS_TO_WRITE(ERDS):
+    ERDS_LIST = []
+
+    Erd_CurrentSystemState = ERDS[0]
     Erd_CurrentSystemState = {                                           
         0: "SystemState_StartUp",
         1: "SystemState_Idle",
@@ -11,8 +13,10 @@ def DATA_TO_WRITE(Data):
         7: "SystemState_DelayPause",
         8: "SystemState_Commissioning",
         9: "SystemState_Max"                               
-    }.get(Erd_CurrentSystemState)  
-    Erd_CycleSelected = Data[1]
+    }.get(Erd_CurrentSystemState)
+    ERDS_LIST.append(Erd_CurrentSystemState)
+
+    Erd_CycleSelected = ERDS[1]
     Erd_CycleSelected = {
         0:  "CycleSelected_NotDefined",
         1:  "CycleSelected_BasketClean",
@@ -62,6 +66,73 @@ def DATA_TO_WRITE(Data):
         146: "CycleSelected_TimedDryWithNoHeat",
         147: "CycleSelected_PermPress",
         148: "CycleSelected_WasherLink"
-    }
+    }.get(Erd_CycleSelected)
+    ERDS_LIST.append(Erd_CycleSelected)
+    
+    Erd_EStarSensorDryRequested = ERDS[2]
+    Erd_EStarSensorDryRequested = {
+        "0": "EStarOption_Disabled",
+        "1": "EStarOption_Enabled",
+        "2": "EStarOption_Max",
+        "255": "EStarOption_DontCare"
+    }.get(Erd_EStarSensorDryRequested)
+    ERDS_LIST.append(Erd_EStarSensorDryRequested)
 
-DATA_TO_WRITE([10, 10])
+    Erd_RamCycleHistoryRecord_drynessOptionAtStart = ERDS[3]
+    Erd_RamCycleHistoryRecord_drynessOptionAtStart = {
+        "0": "DrynessOption_Disabled",
+        "1": "DrynessOption_Minimum",
+        "2": "DrynessOption_LessDry",
+        "3": "DrynessOption_Dry",
+        "4": "DrynessOption_MoreDry",
+        "5": "DrynessOption_ExtraDry",
+        "6": "DrynessOption_Max",
+        "255": "DrynessOption_DontCare"
+    }.get(Erd_RamCycleHistoryRecord_drynessOptionAtStart)
+    ERDS_LIST.append(Erd_RamCycleHistoryRecord_drynessOptionAtStart)
+
+    Erd_RamCycleHistoryRecord_drynessOptionAtEnd = ERDS[4]
+    Erd_RamCycleHistoryRecord_drynessOptionAtEnd = {
+        "0": "DrynessOption_Disabled",
+        "1": "DrynessOption_Minimum",
+        "2": "DrynessOption_LessDry",
+        "3": "DrynessOption_Dry",
+        "4": "DrynessOption_MoreDry",
+        "5": "DrynessOption_ExtraDry",
+        "6": "DrynessOption_Max",
+        "255": "DrynessOption_DontCare"
+    }.get(Erd_RamCycleHistoryRecord_drynessOptionAtEnd)
+    ERDS_LIST.append(Erd_RamCycleHistoryRecord_drynessOptionAtEnd)
+
+    Erd_RamCycleHistoryRecord_temperatureOptionAtStart = ERDS[5]
+    Erd_RamCycleHistoryRecord_temperatureOptionAtStart = {
+        "0": "TemperatureOption_Disabled",
+        "1": "TemperatureOption_NoHeat",
+        "2": "TemperatureOption_ExtraLow",
+        "3": "TemperatureOption_Low",
+        "4": "TemperatureOption_Medium",
+        "5": "TemperatureOption_High",
+        "6": "TemperatureOption_Max",
+        "255": "TemperatureOption_DontCare"   
+    }.get(Erd_RamCycleHistoryRecord_temperatureOptionAtStart)
+    ERDS_LIST.append(Erd_RamCycleHistoryRecord_temperatureOptionAtStart)
+
+    Erd_RamCycleHistoryRecord_temperatureOptionAtEnd = ERDS[6]
+    Erd_RamCycleHistoryRecord_temperatureOptionAtEnd = {
+        "0": "TemperatureOption_Disabled",
+        "1": "TemperatureOption_NoHeat",
+        "2": "TemperatureOption_ExtraLow",
+        "3": "TemperatureOption_Low",
+        "4": "TemperatureOption_Medium",
+        "5": "TemperatureOption_High",
+        "6": "TemperatureOption_Max",
+        "255": "TemperatureOption_DontCare"       
+    }.get(Erd_RamCycleHistoryRecord_temperatureOptionAtEnd)
+    ERDS_LIST.append(Erd_RamCycleHistoryRecord_temperatureOptionAtEnd)
+
+    Erd_CurrentInletTemperature = ERDS[6]
+    ERDS_LIST.append(Erd_CurrentInletTemperature)
+
+    
+
+    return ERDS_LIST
