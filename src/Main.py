696b92b19af58d78fc5132546f139ecb9b881bdb
LeonelNL <leonelnieto305@gmail.com>
2023-08-07 12:32:50 -0600

    Fix changes CountEoC

--- src/Main.py
+++ src/Main.py
@@ -93,14 +93,13 @@ def main():
     while True:
         SetBoard()
         State = ReadERD("C0", "F01B")
-        
         if State != System_State:
             FileCsv.Write_Data_System_State(file_System_State, definitions.System_State(State))
 
         System_State = State
         
-        if State != "05":
-            Count_EndOfCycle = 0
+        if State not in ["03", "04", "05"]:
+            FirstCall = True
         
         if (State == "03") or (State == "04") or (System_State == "05" and Count_EndOfCycle == 0):
             tiempo_actual = time.time()
@@ -123,15 +122,18 @@ def main():
 
             DATA_TO_WRITE = [Erd_CurrentSystemState, Erd_CycleSelected, Erd_EStarSensorDryRequested, Erd_RamCycleHistoryRecord_drynessOptionAtStart, Erd_RamCycleHistoryRecord_drynessOptionAtEnd, Erd_RamCycleHistoryRecord_temperatureOptionAtStart, Erd_RamCycleHistoryRecord_temperatureOptionAtEnd, Erd_CurrentInletTemperature,Erd_CurrentOutletTemperature, Erd_OverTemperatureMaxInletTemperature, Erd_HeaterRelay1, Erd_HeaterRelay2, Erd_MaxTemperatureSlope, Erd_HeatControlParametric, Erd_MinimumFilteredVoltageFromMc, Erd_FilteredMoistureSensor, Erd_SmoothMoistureReading, Erd_CalculatedCurvature, Erd_CurvatureOccurredCount, Erd_TrimmerInhibitRelay1, Erd_TrimmerInhibitRelay2, Erd_TrimmerBothCoilInhibitRequest, Erd_DrumMotorState, Erd_FallbackHeatControlMethodStatus, Erd_ApplicationVersion, Erd_ParametricVersion, Erd_Personality, Erd_DrynessOption, Erd_VentRestriction, Erd_LoadSizeByAggregation, Erd_LoadSizeByContact, Erd_LoadSizeByTemperature, Erd_TargetMoistureVoltageHasBeenReached, Erd_TargetMoistureVoltage, Erd_TotalDryTimeCalculatorTimeMultiplierX100, Erd_TotalDryTimeCalculatorTimeAdderSeconds, Erd_SensorDryTemperatureMultiplierx100_temperatureMultiplierEcoDry, Erd_SensorDryTemperatureMultiplierx100_temperatureMultiplierExtraLow, Erd_SensorDryTemperatureMultiplierx100_temperatureMultiplierLow, Erd_SensorDryTemperatureMultiplierx100_temperatureMultiplierMedium, Erd_SensorDryTemperatureMultiplierx100_temperatureMultiplierHigh,Erd_TimeToReachTargetVoltageSeconds, Erd_SensingCycleTotalDryingTimeSeconds, Erd_DrumGroundWatchdogResult, Erd_ClothDampnessCheckResult, Erd_Fault_DrumGroundWatchdogDetection, Erd_SteamValveCycleCountRam, Erd_SteamValveOnTimeDurationInSecondsRam, Erd_CoolDownStepStatus, Erd_ExtendedTumbleStepStatus, Erd_SteamStepStatus, Erd_EndOfCycleReason, Erd_ModelNumber, Erd_SerialNumber, Erd_AppliancePersonality, Erd_MachineStatus, Erd_MachineSubCycle, Erd_EcoDryOptionRequest, Erd_ReduceStaticOption, Erd_ExtendedTumbleAllowable, Erd_DetangleAllowable, Erd_MyCycleAllowable]
             
-            if tiempo_actual - tiempo_referencia >= 60:
+            if (tiempo_actual - tiempo_referencia >= 60) or FirstCall:
                 if Erd_CurrentSystemState == "05":
                     Count_EndOfCycle == 1
+                else:
+                    Count_EndOfCycle == 0
                 TimeS = datetime.now().strftime("%H:%M")
                 DiaS = datetime.now().strftime("%d-%m-%Y") 
                 DATA_TO_CSV = [DiaS] + [TimeS] + definitions.ERDS_TO_WRITE(DATA_TO_WRITE)
                 print(DATA_TO_CSV)
                 FileCsv.Write_Data_CSV(File_Data_Erds, DATA_TO_CSV) 
                 tiempo_referencia = tiempo_actual
+                FirstCall = False
 
 if __name__ == "__main__":
     main()
